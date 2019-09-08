8771f84dc55a7404fade745ead27f1e9440af0bd
aleksander <amako9281@gmail.com>
2019-09-08 19:54:31 +0300

    pre pull commit

--- client/docker-bot-env.sh
+++ client/docker-bot-env.sh
@@ -1,6 +1,6 @@
 export BOT_DOCKER_NETWORK_NAME=socket-network
 export LOW=3
 export HIGH=2000
-export PORT=4001
-export PROJECT_NAME=clientapp6
-export CONTAINER_NAME=bot-player6
+export PORT=4000
+export PROJECT_NAME=clientapp
+export CONTAINER_NAME=bot-player
